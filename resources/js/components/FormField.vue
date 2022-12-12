a75887b8c089d222ed412493099070b71cfef9a5
Val Vankov <val@vankov.me>
2022-12-13 01:21:41 +0200

    x

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -391,6 +391,9 @@ export default {
             width: Math.max(320, parseInt(this.width, 10)) || 640,
             height: Math.max(180, parseInt(this.height, 10)) || 480,
           })
+
+          window.preventDefault();
+          return false;
         },
     },
 
